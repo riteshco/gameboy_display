7ec4e98f4419fd8facd4960800ee040dc27119f8
riteshco <riteshgadwalv2@gmail.com>
2026-02-04 12:44:22 +0530

    add rom loading and storing
    Signed-off-by: riteshco <riteshgadwalv2@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,27 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::thread::sleep;
 use std::time::Duration;
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use gb_core::cpu::Cpu;
 
 const SCALE: u32 = 3;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: cargo run path/to/game");
+        return;
+    }
+
+    let mut gb = Cpu::new();
+    let filename = &args[1];
+    let rom = load_rom(filename);
+    gb.load_rom(&rom);
+
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem.window("My GameBoy Emulator", WINDOW_WIDTH, WINDOW_HEIGHT)
@@ -28,4 +43,12 @@ fn main() {
 
         sleep(Duration::from_millis(100));
     }
+}
+
+fn load_rom(path: &str) -> Vec<u8> {
+    let mut buffer: Vec<u8> = Vec::new();
+    let mut f = File::open(path).expect("Error opening rom file");
+
+    f.read_to_end(&mut buffer).expect("Error reading rom file");
+    buffer
 }
